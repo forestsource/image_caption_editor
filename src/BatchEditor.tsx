6763af5a56febbf6672f25ffdd43a372470390da
forestsource <forestsource@users.noreply.github.com>
2024-03-24 23:17:32 +0900

    move EditAllTags

--- src/BatchEditor.tsx
+++ src/BatchEditor.tsx
@@ -8,15 +8,7 @@ import SaveIcon from "@mui/icons-material/Save";
 
 import { DatasetsContext } from "./Contexts/DatasetsContext";
 import { TopNTags } from "./TopNTags";
-
-const isValidRegex = (userInput: string): boolean => {
-  try {
-    new RegExp(userInput);
-  } catch (e) {
-    return false;
-  }
-  return true;
-};
+import { EditAllTags } from "./EditAllTags";
 
 export function BatchEditor() {
   const { state, dispatch } = useContext(DatasetsContext);
@@ -25,68 +17,9 @@ export function BatchEditor() {
   const allTags = Array.from(new Set(flatTags));
   const [filter, setFilter] = React.useState<string>("");
 
-  const filterdTags = () => {
-    if (filter === "" || filter === undefined) {
-      return allTags;
-    }
-    if (!isValidRegex(filter)) {
-      return [];
-    }
-    const regex = new RegExp(filter, "i");
-    return allTags.filter((tag) => {
-      return tag.match(regex);
-    });
-  };
-  const handleDelete = (tag: string) => () => {
-    dispatch({ type: "REMOVE_CAPTION_TAG", payload: tag });
-    console.debug("delete tag: ", datasets[0].caption.content);
-  };
-  const onSaveCaption = () => {
-    datasets.forEach((dataset) => {
-      dispatch({ type: "SAVE_CAPTION", payload: dataset });
-    });
-  };
-
   return (
     <Box>
-      <Paper elevation={3} sx={{ margin: "1em" }}>
-        <Box
-          sx={{
-            margin: "1em",
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <Fab
-            aria-label="save"
-            color="primary"
-            variant="extended"
-            onClick={onSaveCaption}
-            sx={{ margin: 1 }}
-          >
-            <SaveIcon sx={{ mr: 1 }} /> Save All
-          </Fab>
-          <TextField
-            id="tag-filter"
-            label="search"
-            variant="standard"
-            onChange={(event) => setFilter(event.target.value)}
-            sx={{ textAlign: "right" }}
-          />
-        </Box>
-        <Box sx={{ margin: "1em", paddingBottom: "1em" }} id="allTagsBox">
-          {filterdTags().map((tag, index) => (
-            <Chip
-              key={index}
-              label={tag}
-              color={"primary"}
-              variant="outlined"
-              sx={{ margin: "0.1em" }}
-              onDelete={handleDelete(tag)}
-            />
-          ))}
-        </Box>
-      </Paper>
+      <EditAllTags />
       <TopNTags />
     </Box>
   );
