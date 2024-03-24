97f86ff684d0e5906a30e66ca5fe4696ebf16e34
forestsource <forestsource@users.noreply.github.com>
2024-03-25 00:01:02 +0900

    add counter to chip

--- src/EditAllTags.tsx
+++ src/EditAllTags.tsx
@@ -7,8 +7,14 @@ import Fab from "@mui/material/Fab";
 import SaveIcon from "@mui/icons-material/Save";
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
+import { Avatar } from "@mui/material";
 
 import { DatasetsContext } from "./Contexts/DatasetsContext";
+
+interface TagChip {
+  tagName: string;
+  count: number;
+}
 
 const isValidRegex = (userInput: string): boolean => {
   try {
@@ -23,23 +29,39 @@ export function EditAllTags() {
   const { state, dispatch } = useContext(DatasetsContext);
   const datasets = state.datasets;
   const flatTags = datasets.flatMap((dataset) => dataset.caption.content);
-  const allTags = Array.from(new Set(flatTags));
+  const allTags = (): TagChip[] => {
+    const tags = flatTags.reduce((acc, tag) => {
+      if (acc[tag] === undefined) {
+        acc[tag] = 1;
+      } else {
+        acc[tag] += 1;
+      }
+      return acc;
+    }, {} as { [key: string]: number });
+    return Object.entries(tags)
+      .map(([tagName, count]) => ({
+        tagName,
+        count,
+      }))
+      .sort((a, b) => b.count - a.count);
+  };
   const [filter, setFilter] = React.useState<string>("");
   const [onSaveSuccess, setOnSaveSuccess] = React.useState(false);
   const [onSaveFailure, setOnSaveFailure] = React.useState(false);
 
   const filterdTags = () => {
     if (filter === "" || filter === undefined) {
-      return allTags;
+      return allTags();
     }
     if (!isValidRegex(filter)) {
       return [];
     }
     const regex = new RegExp(filter, "i");
-    return allTags.filter((tag) => {
-      return tag.match(regex);
+    return allTags().filter((tag) => {
+      return tag.tagName.match(regex);
     });
   };
+
   const handleDelete = (tag: string) => () => {
     dispatch({ type: "REMOVE_CAPTION_TAG", payload: tag });
     console.debug("delete tag: ", datasets[0].caption.content);
@@ -86,14 +108,17 @@ export function EditAllTags() {
         />
       </Box>
       <Box sx={{ margin: "1em", paddingBottom: "1em" }} id="allTagsBox">
-        {filterdTags().map((tag, index) => (
+        {filterdTags().map((tagChip, index) => (
           <Chip
             key={index}
-            label={tag}
+            label={tagChip.tagName}
             color={"primary"}
             variant="outlined"
-            sx={{ margin: "0.1em" }}
-            onDelete={handleDelete(tag)}
+            avatar={
+              <Avatar sx={{ backgroundColor: "#FFF" }}>{tagChip.count}</Avatar>
+            }
+            sx={{ margin: "0.3em" }}
+            onDelete={handleDelete(tagChip.tagName)}
           />
         ))}
       </Box>
